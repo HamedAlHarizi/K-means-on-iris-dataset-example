f4dd0694d5d255dfa3061b4474938233dcd43b8f
HamedAlHarizi <64221656+HamedAlHarizi@users.noreply.github.com>
2021-09-25 15:21:20 +0400

    Update ir.py

--- ir.py
+++ ir.py
@@ -17,7 +17,7 @@ with open(PATH) as csvfile:
   readCSV = csv.reader(csvfile, delimiter=',')
 
   for i ,row in enumerate(readCSV):
-    #print(row)
+
     irisarray[i,0]=row[0]
     irisarray[i, 1] = row[1]
     irisarray[i, 2] = row[2]
@@ -47,8 +47,8 @@ def inilzClusAnCentroids(irisarray): # as per found data set, Note: centroids ch
       clus2 = np.vstack([clus2, row])
     if (row[4] == 3):
       clus3 = np.vstack([clus3, row])
-
-  centroids[0]= np.average(clus1, axis=0)
+  #recalculate centroids after each iteration
+  centroids[0] = np.average(clus1, axis=0)
   centroids[1] = np.average(clus2, axis=0)
   centroids[2] = np.average(clus3, axis=0)
 
@@ -58,7 +58,7 @@ def inilzClusAnCentroids(irisarray): # as per found data set, Note: centroids ch
 def calculateDistance(irisarray): #to get distance between datapoints and their clasess's centroids
   clus1, clus2, clus3, centroids = inilzClusAnCentroids(irisarray)
   distance = 0
-  count = 0
+
   for row in clus1:
     distance += dist(row, centroids[0])
 
@@ -70,29 +70,12 @@ def calculateDistance(irisarray): #to get distance between datapoints and their 
 
   return distance
 
-def calculatDistance(clus1, clus2, clus3):
-  centroids = inilzClusAnCentroids(irisarray)
-  distance = 0
-  count = 0
-  for row in clus1:
-    distance += dist(row, centroids[0])
-
-  for row in clus2:
-    distance += dist(row, centroids[1])
-
-  for row in clus3:
-    distance += dist(row, centroids[2])
-
-  return distance
-
-
-
 
 
 def Kmeans(irisarray):
   clus1, clus2, clus3, centroids = inilzClusAnCentroids(irisarray)
 
-  #print("distance between datapoints after initial classification = ", origdistance)
+
   clus1 = np.empty([0, 5])
   clus2 = np.empty([0, 5])
   clus3 = np.empty([0, 5])
@@ -128,10 +111,10 @@ def Kmeans(irisarray):
   for row in clus3:
     distance += dist(row, centroids[2])
 
-  #print('distance after one iteration (k-means classification) = ',distance)
-  #print('total items in class 1 = ',len(clus1))
-  #print('total items in class 2',len(clus2))
- # print('total items in class 3',len(clus2))
+  centroids[0] = np.average(clus1, axis=0)
+  centroids[1] = np.average(clus2, axis=0)
+  centroids[2] = np.average(clus3, axis=0)
+
   return clus1,clus2,clus3,distance
 
 
